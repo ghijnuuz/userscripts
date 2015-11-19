cfa89c0c0400ed6f76f495a93200053bb4fb9411
ZhiJun Gu <ghijnuuz@gmail.com>
2015-11-19 22:21:07 +0800

    更新userscript。 添加gitignore。

--- Magnet2Torrent.user.js
+++ Magnet2Torrent.user.js
@@ -1,26 +1,28 @@
 // ==UserScript==
 // @name         Magnet2Torrent
 // @namespace    http://anonymous.com/
-// @version      0.1
+// @version      0.2
 // @description  Magnet2Torrent
 // @author       anonymous
+// @match        http://www.breadsearch.com/*
 // @match        http://btdigg.org/*
-// @match        http://torrentz.eu/*
+// @match        https://thepiratebay.gd/*
+// @match        https://torrentz.eu/*
 // @match        http://extratorrent.cc/*
 // @match        http://bitsnoop.com/*
 // @grant        none
-// @require      http://code.jquery.com/jquery-1.11.2.min.js
+// @require      http://code.jquery.com/jquery-2.1.4.min.js
 // ==/UserScript==
 
-$("a").each(function() {
-	var hrefStr = $(this).attr("href");
-	if (hrefStr && hrefStr.length > 0 && hrefStr.match(/^magnet:\?/i)) {
-		var infoHash = hrefStr.replace(/(.*urn:btih:)([0-9a-f]*)(&.*)/i, "$2").toUpperCase();
-		var newNode = $(this).clone();
-		$(this).after(newNode);
-		var newHrefStr = "http://torcache.net/torrent/{infoHash}.torrent".replace(/{infoHash}/, infoHash);
-		newNode.attr("href", newHrefStr);
-		newNode.attr("target", "_blank");
-		newNode.attr("title", "Magnet2Torrent");
-	}
-})
+$("a").each(function () {
+    var hrefStr = $(this).attr("href");
+    if (hrefStr && hrefStr.length > 0 && hrefStr.match(/^magnet:\?/i)) {
+        var infoHash = hrefStr.replace(/(.*urn:btih:)([0-9a-f]*)(&.*)/i, "$2").toUpperCase();
+        var newNode = $(this).clone();
+        $(this).after(newNode);
+        var newHrefStr = "http://torcache.net/torrent/{infoHash}.torrent".replace(/\{infoHash\}/, infoHash);
+        newNode.attr("href", newHrefStr);
+        newNode.attr("target", "_blank");
+        newNode.attr("title", "Magnet2Torrent");
+    }
+});
--- X-Art Helper.user.js
+++ X-Art Helper.user.js
@@ -1,75 +1,110 @@
 // ==UserScript==
 // @name         X-Art Helper
 // @namespace    http://www.x-art.com/
-// @version      0.3
+// @version      0.4
 // @description  X-Art Helper
 // @author       anonymous
 // @match        http://www.x-art.com/videos/*/
 // @grant        none
-// @require      http://code.jquery.com/jquery-1.11.2.min.js
+// @require      http://code.jquery.com/jquery-2.1.4.min.js
 // ==/UserScript==
 
-var XArt = {
-	TorrentSearchEngine: {
-		Engines: [{
-			name: "BTCherry",
-			url: "http://www.btcherry.net/search?keyword={SearchString}"
-		}, {
-			name: "BTDigg",
-			url: "http://btdigg.org/search?q={SearchString}"
-		}, {
-			name: "The Pirate Bay",
-			url: "https://thepiratebay.se/search/{SearchString}"
-		}, {
-			name: "Torrentz",
-			url: "http://torrentz.eu/any?f={SearchString}"
-		}, {
-			name: "ExtraTorrent",
-			url: "http://extratorrent.cc/search/?search={SearchString}"
-		}, {
-			name: "BitSnoop",
-			url: "http://bitsnoop.com/search/all/{SearchString}"
-		}],
-		GetSearchLinkArray: function(searchStr) {
-			var result = new Array();
-			for (var i = 0; i < this.Engines.length; i++) {
-				var item = this.Engines[i];
-				var str = '<a target="_blank" href="' + item.url.replace(/{SearchString}/, searchStr) + '">' + item.name + '</a>';
-				result.push(str);
-			};
-			return result;
-		}
-	},
-	Videos: {
-		GetVideoInfo: function() {
-			var result = {};
-			result.name = $("#content h1").text();
-			result.date = new Date($("#content .head-list li").eq(0).text());
-			result.models = new Array();
-			$("#content .head-list li a").each(function() {
-				result.models.push($(this).text());
-			});
-			result.GetFileName = function() {
-				var dateNumber = this.date.getFullYear() * 10000 + (this.date.getMonth() + 1) * 100 + this.date.getDate();
-				var modelsStr = this.models.join(" & ");
-				return "X-Art " + dateNumber + " " + modelsStr +  " - " + this.name;
-			};
-			result.GetSearchString = function() {
-				var modelsStr = this.models.join(" ");
-				return "X-Art " + modelsStr + " " + this.name;
-			};
-			return result;
-		},
-		DoHelp: function() {
-			var videoInfo = XArt.Videos.GetVideoInfo();
-			// 添加输入框，写入文件名
-			$("#content .box").eq(0).before('<div><input type="text" style="width: 100%;" id="filenametext" /></div>');
-			$("#filenametext").val(videoInfo.GetFileName());
-			// 添加下载按钮
-			var searchLinkArray = XArt.TorrentSearchEngine.GetSearchLinkArray(videoInfo.GetSearchString());
-			$("#content .box").eq(0).before('<div>DownLoad: ' + searchLinkArray.join(" | ") + '</div>');
-		}
-	}
-};
+/**
+ * XArt 助手
+ * @type {{DoHelp}}
+ */
+var XArtHelper = (function () {
+    /**
+     * 种子搜索引擎
+     * @type {{Engines: *[], GetSearchLinkArray: TorrentSearchEnginer.GetSearchLinkArray}}
+     */
+    var TorrentSearchEnginer = {
+        Engines: [{
+            name: "BreadSearch",
+            url: "http://www.breadsearch.com/search/{SearchString}"
+        }, {
+            name: "BTDigg",
+            url: "http://btdigg.org/search?q={SearchString}"
+        }, {
+            name: "The Pirate Bay",
+            url: "https://thepiratebay.gd/search/{SearchString}"
+        }, {
+            name: "Torrentz",
+            url: "https://torrentz.eu/any?f={SearchString}"
+        }, {
+            name: "ExtraTorrent",
+            url: "http://extratorrent.cc/search/?search={SearchString}"
+        }, {
+            name: "BitSnoop",
+            url: "http://bitsnoop.com/search/all/{SearchString}"
+        }],
+        /**
+         * 获取搜索链接的数组
+         * @param searchStr
+         * @returns {Array}
+         * @constructor
+         */
+        GetSearchLinkArray: function (searchStr) {
+            var result = [];
+            for (var i = 0; i < this.Engines.length; i++) {
+                var item = this.Engines[i];
+                var str = '<a target="_blank" style="color:#fff;" href="' + item.url.replace(/\{SearchString\}/, searchStr) + '">' + item.name + '</a>';
+                result.push(str);
+            }
+            return result;
+        }
+    };
+    var Videos = {
+        /**
+         * 获取视频信息
+         * @returns {{}}
+         * @constructor
+         */
+        GetVideoInfo: function () {
+            var result = {};
+            result.name = $(".wrapper .info h1").text();
+            result.date = new Date($(".wrapper .row .info h2").eq(2).text());
+            result.models = [];
+            $(".wrapper .row .info h2 a").each(function () {
+                result.models.push($(this).text());
+            });
+            /**
+             * 获取文件名
+             * @returns {string}
+             * @constructor
+             */
+            result.GetFileName = function () {
+                var dateNumber = this.date.getFullYear() * 10000 + (this.date.getMonth() + 1) * 100 + this.date.getDate();
+                var modelsStr = this.models.join(" & ");
+                return "X-Art " + dateNumber + " " + modelsStr + " - " + this.name;
+            };
+            /**
+             * 获取搜索内容
+             * @returns {string}
+             * @constructor
+             */
+            result.GetSearchString = function () {
+                var modelsStr = this.models.join(" ");
+                return "X-Art " + modelsStr + " " + this.name;
+            };
+            return result;
+        },
+        /**
+         * 进行帮助
+         * @constructor
+         */
+        DoHelp: function () {
+            var videoInfo = Videos.GetVideoInfo();
+            var inputHtml = '<input type="text" style="width: 100%;" value="' + videoInfo.GetFileName() + '" />';
+            var searchLinkArray = TorrentSearchEnginer.GetSearchLinkArray(videoInfo.GetSearchString());
+            var searchHtml = '<p>Download: ' + searchLinkArray.join(" | ") + '</p>';
+            var addHtml = '<div class="row collapse">' + inputHtml + '<br/>' + searchHtml + '</div>';
+            $(".wrapper .row .info .group-btns").before(addHtml);
+        }
+    };
+    return {
+        DoHelp: Videos.DoHelp
+    };
+})();
 
-XArt.Videos.DoHelp();
+XArtHelper.DoHelp();
